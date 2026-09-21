966c2d7a2a9252611df13addfae04a40d8af3a63
Aarn <koyan.cd@gmail.com>
2026-09-21 17:26:09 -0500

    Online status

--- server.js
+++ server.js
@@ -24,12 +24,11 @@ io.on('connection', (socket) => {
     socket.data.nickname = nickname;
     socket.data.room = room;
 
+    // Notify others
     socket.to(room).emit('user-joined', { nickname });
 
-    const users = [...io.sockets.adapter.rooms.get(room) || []]
-      .map(id => io.sockets.sockets.get(id)?.data.nickname)
-      .filter(Boolean);
-
+    // Send updated users list to everyone in the room
+    const users = getUsersInRoom(room);
     io.to(room).emit('room-users', users);
   });
 
@@ -37,28 +36,51 @@ io.on('connection', (socket) => {
     socket.to(data.room).emit('encrypted-message', data);
   });
 
-  // ===== NEW: Typing indicators =====
   socket.on('typing', () => {
-    socket.to(socket.data.room).emit('typing', {
-      nickname: socket.data.nickname
-    });
-  });
-
-  socket.on('stop-typing', () => {
-    socket.to(socket.data.room).emit('stop-typing', {
-      nickname: socket.data.nickname
-    });
-  });
-  // ==================================
-
-  socket.on('disconnect', () => {
     if (socket.data.room) {
-      socket.to(socket.data.room).emit('user-left', {
+      socket.to(socket.data.room).emit('typing', {
         nickname: socket.data.nickname
       });
     }
   });
+
+  socket.on('stop-typing', () => {
+    if (socket.data.room) {
+      socket.to(socket.data.room).emit('stop-typing', {
+        nickname: socket.data.nickname
+      });
+    }
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.data.room) {
+      const room = socket.data.room;
+      const nickname = socket.data.nickname;
+
+      // Notify others that the user left
+      socket.to(room).emit('user-left', { nickname });
+
+      // Send the updated users list
+      const users = getUsersInRoom(room);
+      io.to(room).emit('room-users', users);
+    }
+  });
 });
+
+// Helper function to get current users in a room
+function getUsersInRoom(room) {
+  const clients = io.sockets.adapter.rooms.get(room) || new Set();
+  const users = [];
+
+  for (const clientId of clients) {
+    const clientSocket = io.sockets.sockets.get(clientId);
+    if (clientSocket && clientSocket.data.nickname) {
+      users.push(clientSocket.data.nickname);
+    }
+  }
+
+  return users;
+}
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
